0e9549c9a069b2533d680f7919cc661c88aae9f8
Haegem <J_gebauer@hotmail.com>
2022-03-03 17:35:06 -0300

    Atualizado

--- Menu.java
+++ Menu.java
@@ -1,37 +1,27 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.HeadlessException;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Color;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
 import javax.swing.JScrollBar;
 import javax.swing.JButton;
-import java.awt.SystemColor;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.awt.event.ActionEvent;
 
 public class Menu extends JFrame {
@@ -59,75 +49,62 @@ public class Menu extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * @throws IOException 
-	 * @throws FileNotFoundException 
-	 * @throws HeadlessException 
+	 * 
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 * @throws HeadlessException
 	 */
-	
-	// Método para salvar nome no ranking
-	public void salvar(String conteudo, boolean adicionar) throws IOException {
-		FileWriter fw = new FileWriter("C:\\Users\\João\\eclipse-workspace\\Jogo-da-velha.txt", adicionar);
-		PrintWriter pw = new PrintWriter(fw);
-		pw.println(conteudo);
-		pw.flush();
-		pw.close();
+
+	public static String carregar(String path) throws IOException {
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		String linha = "";
+		linha = buffRead.readLine();
+		buffRead.close();
+		return linha;
 	}
 
-	public String carregar() throws FileNotFoundException, IOException {
-
-		File file = new File("C:\\Users\\João\\eclipse-workspace\\Jogo-da-velha.txt");
-
-		if (!file.exists()) {
-			return null;
-		}
-
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\João\\eclipse-workspace\\Jogo-da-velha.txt"));
-		StringBuffer bufSaida = new StringBuffer();
-
-		int c;
-		while ((c = br.read()) > 1) {
-			bufSaida.append((char) c);
-		}
-		br.close();
-		return bufSaida.toString();
+	public static void salvar(String path, String linha) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+		buffWrite.append(linha + "-");
+		buffWrite.close();
 	}
-	
+
 	public Menu() throws HeadlessException, FileNotFoundException, IOException {
-		
+
 		String[] rankingName = new String[10];
 		// Salvador player
 		String dataNick = "";
 		int setSave = 0;
-		
+
 		setBounds(100, 100, 700, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
-		//INICIALIZAÇÃO DO BANCO FAKE(VETOR)
-		
-		//OPÇÕES DE DIFICULDADE
+
+		// INICIALIZAÇÃO DO BANCO FAKE(VETOR)
+
+		// OPÇÕES DE DIFICULDADE
 		JComboBox comboBoxDiff = new JComboBox();
 		comboBoxDiff.setBounds(5, 104, 74, 31);
-		comboBoxDiff.setModel(new DefaultComboBoxModel(new String[] {"EASY", "HARD"}));
+		comboBoxDiff.setModel(new DefaultComboBoxModel(new String[] { "EASY", "HARD" }));
 		comboBoxDiff.setFont(new Font("Txt_IV50", Font.BOLD, 15));
 		comboBoxDiff.setToolTipText("DIFF");
-		
+
 		JLabel lblSelectDiff = new JLabel("Selecione a dificuldade:");
 		lblSelectDiff.setFont(new Font("Txt_IV50", Font.PLAIN, 11));
 		lblSelectDiff.setToolTipText("SELECTDIFF");
 		lblSelectDiff.setBounds(5, 79, 172, 14);
 		contentPane.add(lblSelectDiff);
-		//Desabilitar o botão dificuldade
+		// Desabilitar o botão dificuldade
 		comboBoxDiff.setEnabled(false);
-		
-		//OPÇÕES MULTI-SINGLE PLAYERS
+
+		// OPÇÕES MULTI-SINGLE PLAYERS
 		JButton btnMultijogador = new JButton("MULTIJOGADOR");
 		JButton btnUmJogador = new JButton("UM JOGADOR");
-		
+
 		btnMultijogador.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//Desabilitar o botão um jogador
+				// Desabilitar o botão um jogador
 				btnUmJogador.setEnabled(false);
 			}
 		});
@@ -136,12 +113,12 @@ public class Menu extends JFrame {
 		btnMultijogador.setBackground(new Color(154, 205, 50));
 		btnMultijogador.setBounds(412, 109, 128, 23);
 		contentPane.add(btnMultijogador);
-		
+
 		btnUmJogador.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//Desabilitar o botão multijogador
+				// Desabilitar o botão multijogador
 				btnMultijogador.setEnabled(false);
-				//Habilitar o botão dificuldade
+				// Habilitar o botão dificuldade
 				comboBoxDiff.setEnabled(true);
 			}
 		});
@@ -150,8 +127,8 @@ public class Menu extends JFrame {
 		btnUmJogador.setBackground(new Color(154, 205, 50));
 		btnUmJogador.setBounds(274, 109, 128, 23);
 		contentPane.add(btnUmJogador);
-		
-		//TÍTULO
+
+		// TÍTULO
 		JLabel lblTitle = new JLabel("JOGO DA VELHA");
 		lblTitle.setBounds(5, 11, 674, 56);
 		lblTitle.setBackground(Color.ORANGE);
@@ -159,19 +136,18 @@ public class Menu extends JFrame {
 		lblTitle.setFont(new Font("Txt_IV50", Font.BOLD, 40));
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setToolTipText("TITLE");
-		
-		//TÍTULO PARA DIGITAR NICK
+
+		// TÍTULO PARA DIGITAR NICK
 		JLabel lblTitleNick = new JLabel("Digite seu nick:");
 		lblTitleNick.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitleNick.setFont(new Font("Txt_IV50", Font.PLAIN, 25));
 		lblTitleNick.setBounds(262, 150, 278, 41);
 		lblTitleNick.setToolTipText("TITLENICK");
 		contentPane.setLayout(null);
-		
-		
-		//ÁREA PARA DIGITAR O NICK
+
+		// ÁREA PARA DIGITAR O NICK
 		txtNick = new JTextField();
-		txtNick.setBackground(new Color(1.0f,1.0f,1.0f,0.5f));
+		txtNick.setBackground(new Color(1.0f, 1.0f, 1.0f, 0.5f));
 		txtNick.setForeground(Color.BLACK);
 		txtNick.setBounds(196, 220, 429, 56);
 		txtNick.setHorizontalAlignment(SwingConstants.CENTER);
@@ -179,7 +155,7 @@ public class Menu extends JFrame {
 		txtNick.setToolTipText("NICK");
 		txtNick.setColumns(10);
 		contentPane.add(txtNick);
-		
+
 		txtInsiraAqui = new JTextField();
 		txtInsiraAqui.setEditable(false);
 		txtInsiraAqui.setEnabled(false);
@@ -191,27 +167,28 @@ public class Menu extends JFrame {
 		txtInsiraAqui.setColumns(10);
 		txtInsiraAqui.setBounds(196, 220, 429, 56);
 		contentPane.add(txtInsiraAqui);
-		
-		//BOTÃO PARA CONFIRMAR NICK
+
+		// BOTÃO PARA CONFIRMAR NICK
 		JButton btnConfirm = new JButton("CONFIRMAR");
 		btnConfirm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				metaData meta = new metaData();
-				
-				meta.salvar(txtNick.getText(), true);
-				//Players player = new Players();
-				//player.setNick(nick);
-				
-				 if(btnUmJogador.isEnabled() == false) {
-						Game screenGM = new Game();
-						
-						screenGM.setVisible(true);
-						dispose();
-						screenGM.setResizable(false);
-						screenGM.setLocationRelativeTo(null);
-				 }else {
-					 if(comboBoxDiff.getSelectedItem() == "EASY"){
+
+				try {
+					salvar("C:\\Users\\João\\eclipse-workspace\\Jogo-da-velha\\SavedDataNicks.txt", txtNick.getText());
+				} catch (IOException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+
+				if (btnUmJogador.isEnabled() == false) {
+					Game screenGM = new Game();
+
+					screenGM.setVisible(true);
+					dispose();
+					screenGM.setResizable(false);
+					screenGM.setLocationRelativeTo(null);
+				} else {
+					if (comboBoxDiff.getSelectedItem() == "EASY") {
 						JGameEasy screenE = null;
 						try {
 							screenE = new JGameEasy();
@@ -219,13 +196,12 @@ public class Menu extends JFrame {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
-							
+
 						screenE.setVisible(true);
 						dispose();
 						screenE.setResizable(false);
 						screenE.setLocationRelativeTo(null);
-					 }
-					 else {
+					} else {
 						JGameHard screenH = null;
 						try {
 							screenH = new JGameHard();
@@ -233,14 +209,14 @@ public class Menu extends JFrame {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
-						
+
 						screenH.setVisible(true);
 						dispose();
 						screenH.setResizable(false);
 						screenH.setLocationRelativeTo(null);
-				 	}
-				 }
-				
+					}
+				}
+
 			}
 		});
 		btnConfirm.setBackground(new Color(154, 205, 50));
@@ -248,15 +224,21 @@ public class Menu extends JFrame {
 		btnConfirm.setToolTipText("CONFIRM");
 		btnConfirm.setBounds(364, 300, 128, 23);
 		contentPane.add(btnConfirm);
-		
-		//LISTA DO RANKING
+
+		// LISTA DO RANKING
+		String aux = carregar("C:\\Users\\João\\eclipse-workspace\\Jogo-da-velha\\SavedDataNicks.txt");
+		rankingName = aux.split("-");
+
 		JList listRanking = new JList();
 		listRanking.setFont(new Font("Txt_IV50", Font.PLAIN, 11));
 		listRanking.setModel(new AbstractListModel() {
+			
 			String[] values = new String[] {rankingName[0]};
+
 			public int getSize() {
 				return values.length;
 			}
+
 			public Object getElementAt(int index) {
 				return values[index];
 			}
@@ -271,6 +253,6 @@ public class Menu extends JFrame {
 		contentPane.add(comboBoxDiff);
 		contentPane.add(lblTitleNick);
 		contentPane.add(lblTitle);
-		
+
 	}
 }
